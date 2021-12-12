e1ae9071c1d0146fd3469fdf1348b8acc8ad1b7f
TaylorsTheorem <erdem@akar.de>
2021-12-12 18:48:52 +0100

    Set object variables

--- districts.py
+++ districts.py
@@ -1,7 +1,22 @@
+
+''' Class for objects containing all important information about each district '''
+
 class District:
 
-    def __init__(self, data, district_list):
-        set_attr(data, district_list)
+    def __init__(self, data):
+        self.name = data["Name"]
+        self.id = data["AdmUnitId"]
 
-    def set_attr(data, district_list):
-        return
+        self.total_cases = data["AnzFall"]
+        self.total_deaths = data["AnzTodesfall"]
+        self.total_recovered = data["AnzGenesen"]
+        self.total_active = data["AnzAktiv"]
+
+        self.new_cases = data["AnzFallNeu"]
+        self.new_deaths = data["AnzTodesfallNeu"]
+        self.new_recovered = data["AnzGenesenNeu"]
+        self.new_active = data["AnzAktivNeu"]
+
+        self.week_cases = data["AnzFall7T"]
+
+        self.incidence = data["Inz7T"]
